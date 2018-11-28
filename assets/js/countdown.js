8d4d2487c4f01f12d9ec40d55095ef1afb75ab2e
Norman Ramadhan <normanramadhan3@gmail.com>
2018-11-29 03:58:27 +0700

    first commit

---
+++ assets/js/countdown.js
@@ -0,0 +1,35 @@
+// Set the date we're counting down to   
+
+var day = new Date();
+console.log(day);
+
+var nextDay = new Date(day);
+nextDay.setDate(day.getDate() + 1);
+console.log(nextDay);
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = nextDay - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      document.getElementById("countdown").innerHTML = hours + " Jam " +
+            minutes + " Menit " + seconds + " Detik ";
+
+      // If the count down is over, write some text 
+      if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+      }
+}, 1000);
+
---
+++ assets/js/function.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+   $('#image-gallery').lightSlider({
+      gallery: true,
+      item: 1,
+      thumbItem: 9,
+      slideMargin: 0,
+      speed: 500,
+      auto: true,
+      loop: true,
+      onSliderLoad: function () {
+         $('#image-gallery').removeClass('cS-hidden');
+      }
+   });
+});
